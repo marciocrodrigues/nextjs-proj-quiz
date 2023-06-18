d9dcc0915564e7502d7b922f933f3e8474072a46
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-06-18 00:27:12 -0300

    componente botao e questionario

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Questao from "../components/Questao";
 import QuestaoModel from "../model/questao";
 import RespostaModel from "../model/resposta";
+import Botao from "../components/Botao";
+import Questionario from "../components/Questionario";
 
 const questaoMock = new QuestaoModel(1, "Melhor cor?", [
   RespostaModel.errada("Verde"),
@@ -13,29 +15,25 @@ const questaoMock = new QuestaoModel(1, "Melhor cor?", [
 export default function Home() {
   const [questao, setQuestao] = useState(questaoMock);
 
-  function respostaFornecida(indice: number) {
-    setQuestao(questao.responderCom(indice));
-  }
+  function questaoRespondida(questao: QuestaoModel) {}
 
-  function tempoEsgotado() {
-    if (questao.naoRespondida) {
-      setQuestao(questao.responderCom(-1));
-    }
-  }
+  function irParaProximoPasso() {}
 
   return (
     <div
       style={{
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
       }}
     >
-      <Questao
-        valor={questao}
-        respostaFornecida={respostaFornecida}
-        tempoEsgotado={tempoEsgotado}
+      <Questionario
+        questao={questao}
+        ultima={false}
+        questaoRespondida={questaoRespondida}
+        irParaProximoPasso={irParaProximoPasso}
       />
     </div>
   );
